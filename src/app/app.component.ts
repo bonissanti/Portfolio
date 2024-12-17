f72380eb466195c661922a9f2323a3694c440474
Bruno Onissanti <brunoonissanti@gmail.com>
2024-12-16 22:23:55 -0300

    feat: added cards component
    This will be used by the portfolio and blog sections

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from "./shared/button/button.component";
+import { CardsComponent } from "./shared/cards/cards.component";
 import { HeaderComponent } from "./header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ButtonComponent, HeaderComponent],
+  imports: [ButtonComponent, HeaderComponent, CardsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/shared/cards/cards.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-cards',
+  imports: [],
+  templateUrl: './cards.component.html',
+  styleUrl: './cards.component.scss'
+})
+export class CardsComponent {
+
+}

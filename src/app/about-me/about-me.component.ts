1771338ccea475049435e1ea11c73428b06df35b
Bruno Onissanti <brunoonissanti@gmail.com>
2024-12-17 21:36:02 -0300

    feat: new about-me component
    A bio + profile pic and future emoji

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ButtonComponent } from "../shared/button/button.component";
+import { NgIf } from '@angular/common';
 
 @Component({
   standalone: true,
   selector: 'app-about-me',
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, NgIf],
   templateUrl: './about-me.component.html',
-  styleUrl: './about-me.component.scss'
+  styleUrls: ['./about-me.component.scss', '../shared/button/button.component.scss']
 })
 export class AboutMeComponent {
-
+  @Input() profilePic = '';
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { ButtonComponent } from "./shared/button/button.component";
 import { CardsComponent } from "./shared/cards/cards.component";
 import { HeaderComponent } from "./header/header.component";
+import { AboutMeComponent } from "./about-me/about-me.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ButtonComponent, HeaderComponent, CardsComponent],
+  imports: [ButtonComponent, HeaderComponent, AboutMeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
